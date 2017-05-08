fec80329bb98874e43a1ec3d8b3a022aecf2bce6
Jasper Fung <jsprfng@gmail.com>
2017-05-07 21:58:19 -0700

    init jasmine, eslint, travis-ci, and scripts

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "rules": {
+    	"semi": ["error", "never"],
+    	"no-tabs": 0,
+    	"indent": 0
+    }
+};
